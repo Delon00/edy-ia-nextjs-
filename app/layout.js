17617933345ee8ac6fda8af989ae34d9d0d07bd4
Delon00 <djplebob32@gmail.com>
2024-03-03 21:09:58 +0000

    modified:   .gitignore 	modified:   app/layout.js 	modified:   next.config.mjs 	modified:   package-lock.json 	modified:   package.json 	new file:   public/icon-192x192.png 	new file:   public/icon-256x256.png 	new file:   public/icon-384x384.png 	new file:   public/icon-512x512.png 	new file:   public/manifest.json

--- app/layout.js
+++ app/layout.js
@@ -7,7 +7,22 @@ const inter = Inter({ subsets: ["latin"] });
 export const metadata = {
   title: "Edy-ia",
   description: "Edy-ia",
+  generator: "Next.js",
+  manifest: "/manifest.json",
+  keywords: ["Cours ia", "Répétiteur ia", "Ia", "Ecole"],
+  authors: "Delon Jean-Philippe",
+  icons: [
+    { rel: "apple-touch-icon", url: "icons/icon-128x128.png" },
+    { rel: "icon", url: "icons/icon-128x128.png" },
+  ],
 };
+
+export const themeColor = [{ media: "(prefers-color-scheme: #f69435)", color: "#fff" }];
+
+export const viewport =
+  "minimum-scale=1, initial-scale=1, width=device-width, shrink-to-fit=no, viewport-fit=cover";
+
+
 
 export default function RootLayout({ children }) {
   return (
