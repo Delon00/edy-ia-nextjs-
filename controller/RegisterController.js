fb547eeb38272c6f7f6cf4cbeb0cdf46f54de44d
Delon00 <djplebob32@gmail.com>
2024-03-10 12:24:07 +0000

    modified:   controller/RegisterController.js 	modified:   package-lock.json 	modified:   package.json

--- controller/RegisterController.js
+++ controller/RegisterController.js
@@ -1,16 +1,31 @@
+'use server'
 import { RegisterSchema } from "@/schemas";
+import  prisma  from "@/lib/prisma";
+import bcrypt from 'bcrypt';
 
-export const RegisterAction = (values) => {
+export const RegisterAction = async (values) => {
     console.log("Données du formulaire :", values);
     const validationResult = RegisterSchema.safeParse(values);
 
     if (!validationResult.success) {
         return { error: "Champs invalides", details: validationResult.error };
-        // Ajout des détails de l'erreur pour faciliter le débogage
     }
 
-    // Logique supplémentaire si nécessaire (par exemple, envoi d'un email)
+    const { email, password } = validationResult.data;
+    const hashedPassword = await bcrypt.hash(password, 10);
+    const existingUser = await prisma.User.findUnique({ where: { email } });
+    
+    if (existingUser) {
+        return { error: "Cette adresse est déjà inscrite" };
+    }
 
-    return { success: "Email envoyé" };
+    await prisma.user.create({
+        data: {
+            email,
+            password: hashedPassword,
+        },
+    });
+
+    return { success: "Compte créé" };
 };
 
